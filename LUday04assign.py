6b15c0b8eb53b209b8600e34e12ec08e188c965a
KunalVatwani <86547630+KunalVatwani@users.noreply.github.com>
2021-07-12 14:42:32 +0530

    Update LUday04assign.py

--- LUday04assign.py
+++ LUday04assign.py
@@ -5,18 +5,18 @@
 #A2). It is used in making bar graphs, line plots, pie charts, scatter plots, etc and simplifies the data analysation process.
 
 #Q3).Make a pie chart of india's car companies market share % value
-# import plotly.graph_objects as go
-# company = ['Maruti Suzuki', 'Hyundai', 'Tata', 'Kia', 'Mahindra', 'Renault']
-# shares = [50,17.4,7,5.8,5.6,3.3]
-# fig = go.Figure(data=(go.Pie(labels = company, values = shares)))
-# fig.show()
+import plotly.graph_objects as go
+company = ['Maruti Suzuki', 'Hyundai', 'Tata', 'Kia', 'Mahindra', 'Renault']
+shares = [50,17.4,7,5.8,5.6,3.3]
+fig = go.Figure(data=(go.Pie(labels = company, values = shares)))
+fig.show()
 
 #Q4). plot a bar chart with india's covid 19 active cases
-# import plotly.express as px
-# states = ['Maharashtra','Kerela','Karnataka','Tamil Nadu','Andhra Pradseh','Uttar Pradesh']
-# cases = [119442, 115327, 36760, 32307, 28680, 1594]
-# fig = px.bar(x=states, y=cases)
-# fig.show()
+import plotly.express as px
+states = ['Maharashtra','Kerela','Karnataka','Tamil Nadu','Andhra Pradseh','Uttar Pradesh']
+cases = [119442, 115327, 36760, 32307, 28680, 1594]
+fig = px.bar(x=states, y=cases)
+fig.show()
 
 #Q5). plot a line chart with india's active covid 19 case recovery
 import plotly.express as px
